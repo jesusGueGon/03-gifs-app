a8944fc35d961032d79906d544dbda7f6a12652f
jesusGueGon <jesus.guerrero@bosonit.com>
2023-05-09 16:50:55 +0200

    Seccion 8 Completada

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -11,10 +13,12 @@ import { CardListComponent } from './components/card-list/card-list.component';
   declarations: [
     HomePageComponent,
     ShearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
