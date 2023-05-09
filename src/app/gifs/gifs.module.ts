f419db6d72f4ac80a989ce733853a627734ce3ce
jesusGueGon <jesus.guerrero@bosonit.com>
2023-05-09 15:30:59 +0200

    seccion 7 completada

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    ShearchBoxComponent
+    ShearchBoxComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
